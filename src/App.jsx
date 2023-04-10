26b4e0935d5403f4247a94232635e508f3115c8a
Tae-kyun Kim <ktk03115@gmail.com>
2023-04-10 19:37:03 +0900

    Update some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css';
 import { useState, useRef } from 'react';
 
-let nameInput = "이름";
-let postPosition;
+// let postPosition;
 
 function App() {
 
-  postPosition = checkBatchim(nameInput) ? '과' : '와';
+  //postPosition = checkBatchim(nameInput) ? '와' : '과';
 
   return (
     <div className="App">
-      <div className="main-title">
+      <div className="mainTitle">
         <p>
           <h1>'지지를 철회한다' 밈 생성기</h1>
           <h2>jiji-stop generator</h2>
@@ -20,7 +19,7 @@ function App() {
       <div className="list">
         <h3>사이트 설명</h3>
         <p>
-          일명 '지지를 철회한다' 밈을 생성해주는 사이트입니다.<br /><br />
+          일명 <span style={{ fontWeight: "bolder" }}>'지지를 철회한다'</span> 밈을 생성해주는 사이트입니다.<br /><br />
 
           이 밈에 이름 부분을 일일이 갈아 끼우는 것이 귀찮아서... <br />
           한번에 만들어주는 사이트를 만들자는 아이디어로 개발되었습니다.<br /><br />
@@ -29,7 +28,7 @@ function App() {
         </p>
       </div>
 
-      <Generator/>
+      <Generator />
 
       <div className='output'>
         <button type="button">
@@ -41,33 +40,44 @@ function App() {
   );
 }
 
-function checkBatchim() { // 이름에 받침 여부 체크
-  if (typeof nameInput !== 'string') return null;
+function checkBatchim(name) { // 이름에 받침 여부 체크
+  if (typeof name !== 'string') return null;
+  if (!name) return '';
 
-  let lastLetter = nameInput[nameInput.length - 1];
+  let lastLetter = name[name.length - 1];
   let uni = lastLetter.charCodeAt(0);
 
-  if (uni < 44032 || uni > 55203) return null;
+  if (uni < 44032 || uni > 55203) return '과/와';
 
-  return (uni - 44032) % 28 !== 0;
+  if ((uni - 44032) % 28 === 0) {
+    return '와';
+  }
+  return '과';
 }
 
 function Generator() { // 밈 생성기
   const [nameInput, setName] = useState("");
+  const [nameState, setNameState] = useState(false);
   const textRef = useRef(null);
 
-  let [autoCopy, toggleAutoCopy] = useState(true); // 자동 복사 기본값: 켜짐
+  let [text, setText] = useState("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+  let [autoCopy, setAutoCopy] = useState(true); // 자동 복사 기본값: 켜짐
 
   const memeText = () => {
-    const text =
-      `오늘부로 ${nameInput} 지지를 철회한다.
+    if (!nameInput) {
+      setNameState(false);
+      alert("이름이 입력되지 않았습니다.");
+    } else {
+      setNameState(true);
+      setText(`오늘부로 ${nameInput} 지지를 철회한다.
 
 오늘부터 지지관계에서 벗어나
-${nameInput}${postPosition}`/* 와/과 */ + ` 나는 한몸으로 일체가 된다.
+${nameInput}${checkBatchim(nameInput)} 나는 한몸으로 일체가 된다.
 ${nameInput}에 대한 공격은 나에 대한 공격으로 간주한다.
 
-세상에 70억명의 ${nameInput} 팬이 있다면, 나는 그들 중 한 명일 것이다.
-세상에 1억명의 ${nameInput} 팬이 있다면, 나 또한 그들 중 한 명일 것이다.
+세상에 70억 명의 ${nameInput} 팬이 있다면, 나는 그들 중 한 명일 것이다.
+세상에 1억 명의 ${nameInput} 팬이 있다면, 나 또한 그들 중 한 명일 것이다.
 세상에 천만 명의 ${nameInput} 팬이 있다면, 나는 여전히 그들 중 한 명일 것이다.
 세상에 백 명의 ${nameInput} 팬이 있다면, 나는 아직도 그들 중 한 명일 것이다.
 세상에 한 명의 ${nameInput} 팬이 있다면, 그 사람은 아마도 나일 것이다.
@@ -82,36 +92,23 @@ ${nameInput}, 나의 슬픔.
 ${nameInput}, 나의 안식.
 ${nameInput}, 나의 영혼.
 ${nameInput}, 나.`
-
-    if (!nameInput) {
-      alert("이름이 입력되지 않았습니다.");
-    } else {
-      textRef.current.value = text;
-      textRef.current.select();
-    }
+      )
+    };
 
     if (autoCopy) {
       document.execCommand("copy");
     }
   }
 
-  const toggleList = document.querySelectorAll(".toggleSwitch");
-
-  toggleList.forEach(($toggle) => {
-    $toggle.onClick = () => {
-      $toggle.classList.toggle('active');
-    }
-  });
-
   return (
     <div>
 
       <div className="select">
-        <h4>종류를 선택하세요</h4>
+        <h4>밈 종류 선택</h4>
       </div>
       <div className="select">
         <label>
-          원본 <Info/>
+          원본 <Info />
           <input type="checkbox"
             name="version"
             value="origin"
@@ -121,7 +118,7 @@ ${nameInput}, 나.`
         </label>
 
         <label>
-          호날두 <Info/>
+          호날두 <Info />
           <input
             type="checkbox"
             name="version"
@@ -131,7 +128,7 @@ ${nameInput}, 나.`
         </label>
 
         <label>
-          롤리타 <Info/>
+          롤리타 <Info />
           <input type="checkbox"
             name="version"
             value="lolita"
@@ -149,14 +146,18 @@ ${nameInput}, 나.`
 
       <div className="select">
         <p style={{ margin: "auto 15px" }}>클립보드 <br />자동복사</p>
-        <input type="checkbox" id="autoCopy" defaultChecked hidden></input>
+        <input
+          type="checkbox"
+          id="autoCopy"
+          defaultChecked
+          hidden />
         <label for="autoCopy" className="toggleSwitch">
-          <span className="toggleButton"></span>
+          <span className="toggleButton" />
         </label>
       </div>
 
       <div className="input">
-        <h4 style={{ padding: "10px" }}>이름</h4>
+        <h4 style={{ padding: "8px" }}>이름</h4>
         <input type="text"
           onChange={
             (e) => setName(e.target.value)
@@ -166,8 +167,7 @@ ${nameInput}, 나.`
 
       <div>
         <div className="output">
-          <textarea ref={textRef} rows="23" cols="80" id="result"></textarea>
-          {/* <p>{memeText.text}</p> */}
+          <p>{text}</p>
         </div>
       </div>
 
@@ -175,14 +175,14 @@ ${nameInput}, 나.`
   )
 
   function Info() {
-    return(
-    <div>
-      <img
-        className="info"
-        alt="info"
-        src="img/info.png"
-      ></img>
-    </div>)
+    return (
+      <div>
+        <img
+          className="info"
+          alt="info"
+          src="img/info.png"
+        />
+      </div>)
   }
 }
 

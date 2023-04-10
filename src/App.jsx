523eeaba41fd31edfe29ab4553b36ec428900e1f
ktk-ring <ktk03115@gmail.com>
2023-04-10 17:38:04 +0900

    Change .js file to .jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <div className="list">
         <h3>사이트 설명</h3>
         <p>
-          지지를 철회한다 밈을 생성해주는 사이트입니다.<br /><br />
+          일명 '지지를 철회한다' 밈을 생성해주는 사이트입니다.<br /><br />
 
           이 밈에 이름 부분을 일일이 갈아 끼우는 것이 귀찮아서... <br />
           한번에 만들어주는 사이트를 만들자는 아이디어로 개발되었습니다.<br /><br />
@@ -29,7 +29,7 @@ function App() {
         </p>
       </div>
 
-      <Generator></Generator>
+      <Generator/>
 
       <div className='output'>
         <button type="button">
@@ -84,7 +84,7 @@ ${nameInput}, 나의 영혼.
 ${nameInput}, 나.`
 
     if (!nameInput) {
-      alert("이름이 입력되지 않았습니다!");
+      alert("이름이 입력되지 않았습니다.");
     } else {
       textRef.current.value = text;
       textRef.current.select();
@@ -111,7 +111,7 @@ ${nameInput}, 나.`
       </div>
       <div className="select">
         <label>
-          원본
+          원본 <Info/>
           <input type="checkbox"
             name="version"
             value="origin"
@@ -121,7 +121,7 @@ ${nameInput}, 나.`
         </label>
 
         <label>
-          호날두
+          호날두 <Info/>
           <input
             type="checkbox"
             name="version"
@@ -131,7 +131,7 @@ ${nameInput}, 나.`
         </label>
 
         <label>
-          롤리타
+          롤리타 <Info/>
           <input type="checkbox"
             name="version"
             value="lolita"
@@ -141,14 +141,14 @@ ${nameInput}, 나.`
 
         {/* {
         version
-        ? <p>이름이 입력되지 않았습니다!</p>
+        ? <p>종류가 선택되지 않았습니다.</p>
         : { 종류 선택 코드 입력 }
         } */}
 
       </div>
 
       <div className="select">
-        <p style={{margin:"auto 25px"}}>클립보드 <br />자동복사</p>
+        <p style={{ margin: "auto 15px" }}>클립보드 <br />자동복사</p>
         <input type="checkbox" id="autoCopy" defaultChecked hidden></input>
         <label for="autoCopy" className="toggleSwitch">
           <span className="toggleButton"></span>
@@ -156,18 +156,17 @@ ${nameInput}, 나.`
       </div>
 
       <div className="input">
-        <h4>이름 입력</h4>
+        <h4 style={{ padding: "10px" }}>이름</h4>
         <input type="text"
-        onChange={
-          (e) => setName(e.target.value)
-        } />
+          onChange={
+            (e) => setName(e.target.value)
+          } />
         <button onClick={memeText}>생성하기</button>
       </div>
 
       <div>
         <div className="output">
-          <textarea ref={textRef} rows="23" cols="80" id="result" hidden></textarea>
-          
+          <textarea ref={textRef} rows="23" cols="80" id="result"></textarea>
           {/* <p>{memeText.text}</p> */}
         </div>
       </div>
@@ -175,6 +174,16 @@ ${nameInput}, 나.`
     </div>
   )
 
+  function Info() {
+    return(
+    <div>
+      <img
+        className="info"
+        alt="info"
+        src="img/info.png"
+      ></img>
+    </div>)
+  }
 }
 
 export default App;
